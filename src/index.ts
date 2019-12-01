afaf7f5838e587d24f78f1b8dedbce1147186c84
henriqueyang <henriqueyang17@gmail.com>
2019-12-01 12:17:03 -0300

    deployed to heroku

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import createServer from './server'
 
-const PORT = Number(process.env.PORT) || 5000
+const PORT = Number(process.env.PORT) || 3000
 const server = createServer()
 
 server.listen(PORT, '0.0.0.0', err => {
