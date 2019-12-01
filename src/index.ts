ae3d3db99ad3a542fde26ca3dda00ab4aa22bcfc
henriqueyang <henriqueyang17@gmail.com>
2019-12-01 12:06:14 -0300

    change port

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import createServer from './server'
 // const PORT = process.env.PORT || 3000
 const server = createServer()
 
-server.listen(3000, '0.0.0.0', err => {
+server.listen(3333, '0.0.0.0', err => {
   if (err) throw err
 })
